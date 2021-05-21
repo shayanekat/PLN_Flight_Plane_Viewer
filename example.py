ec151974896c4aba8903fe845719933cd9d3174b
shayanekat <shaykatfsx@gmail.com>
2021-05-21 13:59:06 +0200

    commented example

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pln_parser
 
-data = pln_parser.parse_pln_file('example.pln')
-data = pln_parser.fix_waypoints(data)
-data_processed = pln_parser.simplify_route(data)
-pln_parser.save_kml_file(data_processed)
-# pln_parser.mapview(data)
+data = pln_parser.parse_pln_file('example.pln') # parse data
+data = pln_parser.fix_waypoints(data) # first process
+data_processed = pln_parser.simplify_route(data) # second process
+pln_parser.save_kml_file(data_processed) # generate kml file
+# pln_parser.mapview(data) # view data
