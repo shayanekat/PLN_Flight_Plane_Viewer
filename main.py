eeb87387bd21fdb341923c7837e8ad84c234c703
shayanekat <shaykatfsx@gmail.com>
2021-05-21 14:07:48 +0200

    initialized GUI

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Shayane Katchera
+Main script making the GUI with python tkinter
+"""
+
+from tkinter import *
+import pln_parser as pln
+#%% BACKEND
+
+
+#%% FRONTEND
+root = Tk()
+root.title("PLN file Viewer")
+
+root.mainloop()
